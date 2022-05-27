724a97e6b1d1fa1ab0f9ec3dade6f7908f5861b4
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-05-27 13:44:04 +0700

    immutable list

---
+++ src/main/java/org/example/collections/ImmutableList.java
@@ -0,0 +1,24 @@
+package org.example.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.*;
+
+public class ImmutableList {
+    public static void main(String[] args) {
+        System.out.println(emptyList()); // immutable list kosong
+        System.out.println(singletonList("aaa")); // immutable list 1 element
+
+        var names = new ArrayList<>();
+        names.add("Arif");
+        names.add("Ikhsan");
+        names.add("Udin");
+
+        //noinspection Java9CollectionFactory
+        System.out.println(unmodifiableList(names)); // mutable to immutable
+
+        var immutableNames = List.of("Arif", "Ikhsan", "Udin");
+        System.out.println(immutableNames);  // immutabe list
+    }
+}
