13c9a2a617d8d57818f5189b41b0b39c60d423d5
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-05-27 13:23:28 +0700

    list

---
+++ src/main/java/org/example/collections/ListExample.java
@@ -0,0 +1,18 @@
+package org.example.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+//        List<String> names = new LinkedList<>();
+
+        names.add("Arif");
+        names.add("Ikhsan");
+
+        names.set(0, "Udin");
+        names.remove(1);
+        System.out.println(names.get(0));
+    }
+}
