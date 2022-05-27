1e88a3bf029c8421201bf79fda6a461b374fa1d1
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-05-27 13:03:07 +0700

    array list

---
+++ src/main/java/org/example/collections/ArrayListExample.java
@@ -0,0 +1,32 @@
+package org.example.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Collection<String> names = new ArrayList<>();
+        names.add("Arif");
+        names.add("Ikhsan");
+        names.add("Udin");
+        names.addAll(List.of("Arif", "Ikhsan", "Udin"));
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        System.out.println("remove...");
+
+        names.remove("Arif");
+        names.removeAll(List.of("Ikhsan", "Udin"));
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        System.out.println(names.contains("Arif"));
+        System.out.println(names.containsAll(List.of("Arif", "Ikhsan"))); // harus ada semua
+        System.out.println(names.size());
+    }
+}
